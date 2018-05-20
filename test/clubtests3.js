8b966f515ecb7fc8176b163667772f6bf468d755
Felice Pollano <felice@felicepollano.com>
2018-05-20 10:01:47 +0200

    added test for check if user is already member

---
+++ test/clubtests3.js
@@ -0,0 +1,18 @@
+const Club = artifacts.require("Club");
+
+contract('testSuite3', async (accounts) => {
+  var instance;
+  beforeEach('setup contract for each test', async function () {
+    await Club.deployed();
+    instance = await Club.new()
+  })
+
+  
+it("check correctly if a memmber exists",async () => {
+  //presenter is the contract creator
+  let presenter = accounts[0];
+  await instance.join(presenter,{from:accounts[1],value:web3.toWei(10,"finney")})
+  assert.isTrue(await instance.ismember.call({from:accounts[1]}));
+})
+
+});
