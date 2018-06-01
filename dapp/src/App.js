f890c48f65abbbb41d655c384f83e18ec2f43044
felicepollano <felice@felicepollano.com>
2018-06-02 00:41:58 +0200

    added bootstrap and react strap to the application

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Jumbotron} from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Jumbotron>
+          <h1 className="display-3">Welcome to EtherClub</h1>
+          <p className="lead">A club for Ethereum enthusiastic who want to play with and potentially win some Ether!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur</p>
+          <p className="lead">dance dance and pecu call, feed the food to the horses!</p>
+        </Jumbotron>
       </div>
     );
   }
