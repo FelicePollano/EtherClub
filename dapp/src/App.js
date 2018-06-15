15c4c0475d3f419c778f87472963067aba9d674b
Felice Pollano <felice@felicepollano.com>
2018-06-15 06:41:03 +0200

    added in dapp

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 import {Jumbotron} from 'reactstrap';
 
 class App extends Component {
+
+  componentDidMount(){
+    
+  }
   render() {
     return (
       <div className="App">
+        <div className="row">
+        <div className="col col-md-12">
         <Jumbotron>
           <h1 className="display-3">Welcome to EtherClub</h1>
           <p className="lead">A club for Ethereum enthusiastic who want to play  and potentially win some Ether!</p>
@@ -14,6 +20,8 @@ class App extends Component {
           <p>Every two people invited you will receive back the same amount you used to join, so after two invitation you earn some Ether everytime you manage to convince someone else to join.</p>
           <p className="lead">Game is base on ability, <u>a prize is given to the best seller!</u> After a threshold of member count the best seller will receive the price payed to join times half the count of <b><u>all</u></b> members in the club.</p>
         </Jumbotron>
+        </div>
+        </div>
       </div>
     );
   }
